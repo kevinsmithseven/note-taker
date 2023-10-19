b721c448a4558c83d98000c0518affb501b24c01
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-18 20:18:35 -0500

    updated server.js to include middleware for parsing to json and urlencoded data, static file serving, added GET requests for notes.html and wildcard for index.html, added GET request for /api/notes to return saved notes as json

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+// Import required packages/modules
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json')
 
-
+// Set server port
 const PORT = 3001;
 
+// Import Express framework
 const app = express();
 
+// Middleware for parsing application/json
 app.use(express.json());
+// Middleware for urlencoded data
 app.use(express.urlencoded({ extended: true }));
-
+// enable static file serving
 app.use(express.static('public'));
+
+// GET request returning notes.html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+// GET request to read db.json and return notes as json
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+
+
+
+
+
+
+// Wildcard returns index.html
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// Event listener for port
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
